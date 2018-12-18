cd24a702d9969bd060e9525766bf12cf3bc3f5e7
EvyHaan <evy.haan@gmail.com>
2018-12-18 08:59:33 -0800

    functioning dbs and corresponding schemas

--- schema.sql
+++ schema.sql
@@ -16,3 +16,25 @@ CREATE TABLE weathers (
     location_id INTEGER NOT NULL,
     FOREIGN KEY (location_id) REFERENCES locations (id)
   );
+
+CREATE TABLE IF NOT EXISTS yelps (
+  id SERIAL PRIMARY KEY, 
+  name VARCHAR(255), 
+  image_url VARCHAR(255), 
+  price CHAR(5), 
+  rating NUMERIC(2,1), 
+  url VARCHAR(255), 
+  location_id INTEGER NOT NULL REFERENCES locations(id) 
+);
+
+CREATE TABLE IF NOT EXISTS movies ( 
+  id SERIAL PRIMARY KEY, 
+  title VARCHAR(255), 
+  overview VARCHAR(1000), 
+  average_votes NUMERIC(4,2), 
+  total_votes INTEGER, 
+  image_url VARCHAR(255), 
+  popularity NUMERIC(6,4), 
+  released_on CHAR(10), 
+  location_id INTEGER NOT NULL REFERENCES locations(id) 
+);
